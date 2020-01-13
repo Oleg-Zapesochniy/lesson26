d6f4f23a5b1c469d8b3c46f84fc6733290e4d2cc
Oleg <1konstriktor@gmail.com>
2020-01-13 18:31:16 +0200

    gulp css

--- gulpfile.js
+++ gulpfile.js
@@ -12,17 +12,27 @@ let arrayJsFilesWithPath = arrayJsFiles.map( (item) => {
 
 let gulp = require('gulp');
 let uglify = require('gulp-uglify-es').default;
+let uglifycss = require('gulp-uglifycss');
 let rename = require('gulp-rename');
 let concat = require('gulp-concat');
 let browserSync = require('browser-sync');
 
 gulp.task('minify', ()=> {
-    return gulp.src('js/*.js')
+    return gulp.src(['js/*.js'])
         .pipe(uglify())
         .pipe(rename({ extname: '.min.js' }))
         .pipe(gulp.dest('build'))
         .pipe(browserSync.reload({stream:true}))
 })
+
+gulp.task('minify-css', ()=> {
+    return gulp.src('css/*.css')
+        .pipe(uglifycss())
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({stream:true}))
+})
+
 gulp.task('concat', ()=>{
     return gulp.src(arrayJsFilesWithPath)
         .pipe(concat('main.js'))
@@ -48,10 +58,11 @@ gulp.task('html', () => {
 
 gulp.task('watch', ()=>{
     gulp.watch('js/*.js', gulp.parallel('minify', 'concat'));
+    gulp.watch('css/*.css', gulp.parallel('minify-css'));
     gulp.watch([
         '*.html',
         'data.json'
     ], gulp.parallel('html'))
 })
 
-gulp.task('default', gulp.parallel('watch', 'browserSync'))
+gulp.task('default', gulp.parallel('minify','concat','minify-css', 'watch', 'browserSync'))
